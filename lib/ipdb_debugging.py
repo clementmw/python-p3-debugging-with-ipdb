84fbd3b6ded481913ea83b7fc4bd3abd644bfc74
clementmwamba <clementmwamba23@gmail.com>
2023-12-06 15:11:12 +0300

    completed

--- lib/ipdb_debugging.py
+++ lib/ipdb_debugging.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
+
 import ipdb
 
-def plus_two(num):
-    num + 2
-    return num
+def plus_two(num=3):
+    total = num + 2
+    print(total)
+    return total
+
+plus_two()
+
+
+
+
